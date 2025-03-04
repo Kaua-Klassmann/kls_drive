3b204957c5aedd27548c1b91725a74fe74c6dda4
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-03-04 09:33:52 -0300

    Added style on create account view

--- frontend/src/views/createAccount.rs
+++ frontend/src/views/createAccount.rs
@@ -6,6 +6,17 @@ const CREATE_ACCOUNT_CSS: Asset = asset!("/assets/styling/create_account.css");
 pub fn CreateAccount() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: CREATE_ACCOUNT_CSS }
-        h1 { "Create Account" }
+        div { id: "create_account_view",
+            form {
+                h1 { "Create Account" }
+                input { r#type: "email", placeholder: "Email", required: true }
+                input {
+                    r#type: "password",
+                    placeholder: "Password",
+                    required: true,
+                }
+                button { "Create Account" }
+            }
+        }
     }
 }
