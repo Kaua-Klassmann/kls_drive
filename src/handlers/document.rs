93a45142e10ac88934234325279b05075f1b6ba1
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-03-30 20:41:13 -0300

    Removed variable with document chuncks

--- src/handlers/document.rs
+++ src/handlers/document.rs
@@ -24,90 +24,99 @@ pub async fn upload_document(
 ) -> impl IntoResponse {
     let db = state.db_conn;
 
-    let mut content_type: Option<String> = None;
-    let mut name: Option<String> = None;
-    let mut document_bytes: Vec<u8> = Vec::new();
-
     while let Ok(Some(mut field)) = multipart.next_field().await {
-        if let Some(field_name) = field.name() {
-            if field_name == "document" {
-                if field.file_name().is_some() {
-                    content_type = Some(field.content_type().unwrap().to_string());
-                    name = Some(field.file_name().unwrap().to_string());
+        if field.name().is_none() || field.name().unwrap() != "document" {
+            continue;
+        }
 
-                    while let Ok(Some(chunk)) = field.chunk().await {
-                        document_bytes.extend_from_slice(&chunk);
-                    }
+        if field.file_name().is_none() {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(json!({
+                    "error": "Invalid file"
+                })),
+            );
+        }
+
+        let content_type = field.content_type().unwrap().to_string();
+        let name = field.file_name().unwrap().to_string();
+
+        let document_exists = document::Entity::find()
+            .filter(
+                Condition::all()
+                    .add(document::Column::Name.eq(name.clone()))
+                    .add(document::Column::IdUser.eq(token.user_id)),
+            )
+            .one(db)
+            .await;
+
+        if document_exists.is_err() {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({
+                    "error": "Failed to upload document"
+                })),
+            );
+        }
+
+        if document_exists.unwrap().is_some() {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(json!({
+                    "error": "File already exists"
+                })),
+            );
+        }
+
+        let document = document::ActiveModel {
+            id_user: Set(token.user_id),
+            name: Set(name.clone()),
+            r#type: Set(content_type),
+            ..Default::default()
+        };
+
+        let document_result = document::Entity::insert(document).exec(db).await;
+
+        if document_result.is_err() {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({
+                    "error": "Failed to upload document"
+                })),
+            );
+        }
+
+        let mut file = fs::File::create(format!("./uploads/documents/{}/{}", token.user_id, name))
+            .await
+            .unwrap();
+
+        while let Ok(Some(chunk)) = field.chunk().await {
+            let mut offset = 0;
+            while offset < chunk.len() {
+                let writed = file.write(&chunk[offset..]).await;
+
+                if writed.is_err() {
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json(json!({
+                            "error": "Failed to save document"
+                        })),
+                    );
                 }
+
+                offset += writed.unwrap();
             }
         }
+
+        return (StatusCode::OK, Json(json!({})));
     }
 
-    if content_type.is_none() || name.is_none() || document_bytes.is_empty() {
-        return (
-            StatusCode::BAD_REQUEST,
-            Json(json!({
-                "error": "Invalid file"
-            })),
-        );
-    }
-
-    let document_exists = document::Entity::find()
-        .filter(
-            Condition::all()
-                .add(document::Column::Name.eq(name.clone().unwrap()))
-                .add(document::Column::IdUser.eq(token.user_id)),
-        )
-        .one(db)
-        .await;
-
-    if document_exists.is_err() {
-        return (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({
-                "error": "Failed to upload document"
-            })),
-        );
-    }
-
-    if document_exists.unwrap().is_some() {
-        return (
-            StatusCode::BAD_REQUEST,
-            Json(json!({
-                "error": "File already exists"
-            })),
-        );
-    }
-
-    let document = document::ActiveModel {
-        id_user: Set(token.user_id),
-        name: Set(name.clone().unwrap()),
-        r#type: Set(content_type.unwrap()),
-        ..Default::default()
-    };
-
-    let document_result = document::Entity::insert(document).exec(db).await;
-
-    if document_result.is_err() {
-        return (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({
-                "error": "Failed to upload document"
-            })),
-        );
-    }
-
-    let mut file = fs::File::create(format!(
-        "./uploads/documents/{}/{}",
-        token.user_id,
-        name.unwrap()
-    ))
-    .await
-    .unwrap();
-
-    file.write(&document_bytes).await.unwrap();
-
-    (StatusCode::OK, Json(json!({})))
+    (
+        StatusCode::BAD_REQUEST,
+        Json(json!({
+            "error": "Invalid schema"
+        })),
+    )
 }
 
 #[derive(Deserialize, Validate)]
